f849bc37dbc6d3db15f4a1f7250e60cf8ff8a28e
Dwight McLeish Jr <dmcleishjr@gmail.com>
2023-04-03 13:47:58 -0400

    feat: solution for 11 leetcode

--- src/medium/11.ts
+++ src/medium/11.ts
@@ -3,8 +3,16 @@ function maxArea(height: number[]): number {
   let rightPointer = height.length - 1;
   let max = 0;
 
-  while (leftPointer < rightPointer) {}
-  return 1;
+  while (leftPointer < rightPointer) {
+    if (height[leftPointer] < height[rightPointer]) {
+      max = Math.max(max, height[leftPointer] * (rightPointer - leftPointer));
+      leftPointer++;
+    } else {
+      max = Math.max(max, height[rightPointer] * (rightPointer - leftPointer));
+      rightPointer--;
+    }
+  }
+  return max;
 }
 
 const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
