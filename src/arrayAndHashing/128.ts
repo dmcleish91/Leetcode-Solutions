74d6071eda17b335f88e790b6c6d0491ea65bb92
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-09-21 15:02:31 -0400

    feat: added notes for 128

---
+++ src/arrayAndHashing/128.ts
@@ -0,0 +1,27 @@
+// This function finds the length of the longest consecutive sequence of numbers in an array.
+// Time complexity is O(n)
+function longestConsecutive(nums: number[]): number {
+  // Create a Set to store the input numbers for efficient lookup.
+  const answerSet = new Set(nums);
+  // Initialize variables to keep track of the longest consecutive sequence and the current sequence length.
+  let longest = 0;
+  let length = 0;
+
+  // Iterate through the array of numbers.
+  nums.forEach((number) => {
+    // Check if the current number is the start of a potential consecutive sequence.
+    if (!answerSet.has(number - 1)) {
+      // Reset the current sequence length to 0.
+      length = 0;
+      // Check how long the consecutive sequence can go by incrementing the number.
+      while (answerSet.has(number + length)) {
+        length++;
+      }
+      // Update the longest consecutive sequence if the current sequence is longer.
+      longest = Math.max(length, longest);
+    }
+  });
+
+  // Return the length of the longest consecutive sequence.
+  return longest;
+}
