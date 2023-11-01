4d63af7cf825cc96897b460ddc1301435151f259
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2023-11-01 05:50:14 -0400

    feat: solved searchMatrix again

--- src/binarysearch/SearchA2DMatrix.ts
+++ src/binarysearch/SearchA2DMatrix.ts
@@ -1,40 +1,50 @@
-function searchMatrix(matrix: number[][], target: number): boolean {
-  const rows = matrix.length;
-  const columns = matrix[0].length;
+function searchMatrix(matrix: number[][], target: number) {
+  let left = 0;
+  let right = matrix.length - 1;
 
-  let top = 0;
-  let bottom = rows - 1;
+  while (left <= right) {
+    let row = Math.trunc((left + right) / 2);
 
-  while (top <= bottom) {
-    const middleRow = Math.trunc((top + bottom) / 2);
-    if (target > matrix[middleRow][matrix[0].length - 1]) {
-      top = middleRow + 1;
-    } else if (target < matrix[middleRow][0]) {
-      bottom = middleRow - 1;
+    if (matrix[row][matrix[row].length - 1] > target && matrix[row][0] < target) {
+      break;
+    }
+
+    if (matrix[row][matrix[row].length - 1] > target) {
+      right = row - 1;
+    } else if (matrix[row][0] < target) {
+      left = row + 1;
     } else {
       break;
     }
   }
 
-  if (top > bottom) return false;
-
-  let left = 0;
-  let right = columns - 1;
-  let answerRow = Math.trunc((top + bottom) / 2);
+  const row = Math.trunc((left + right) / 2);
+  left = 0;
+  right = matrix[row].length - 1;
 
   while (left <= right) {
-    const middle = Math.trunc((left + right) / 2);
+    let middle = Math.trunc((left + right) / 2);
+    if (matrix[row][middle] === target) {
+      return true;
+    }
 
-    if (target > matrix[answerRow][middle]) {
-      left = middle + 1;
-    } else if (target < matrix[answerRow][middle]) {
+    if (matrix[row][middle] > target) {
       right = middle - 1;
     } else {
-      return true;
+      left = middle + 1;
     }
   }
 
   return false;
 }
 
-console.log(searchMatrix([[1], [3]], 1));
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    3
+  )
+);
